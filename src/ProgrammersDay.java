ebf9d1f1cc85ef4ddec74501bd8329ac5bb238ba
lDagothUrl <123746336+lDagothUrl@users.noreply.github.com>
2023-01-30 14:29:58 +0300

    Август заменен на Сентябрь

--- src/ProgrammersDay.java
+++ src/ProgrammersDay.java
@@ -3,13 +3,13 @@ public class ProgrammersDay {
     private LocalDate localDate;
     public ProgrammersDay(int year){
         if(year % 400 == 0) {
-            localDate = LocalDate.of(year, 8, 12);
+            localDate = LocalDate.of(year, 9, 12);
         } else if (year % 100 == 0) {
-            localDate = LocalDate.of(year, 8,13);
+            localDate = LocalDate.of(year, 9,13);
         }else if (year % 4 == 0){
-            localDate = LocalDate.of(year, 8, 12);
+            localDate = LocalDate.of(year, 9, 12);
         }else {
-            localDate = LocalDate.of(year, 8, 13);
+            localDate = LocalDate.of(year, 9, 13);
         }
     }
 
